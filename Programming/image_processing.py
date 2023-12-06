2baa11214aeabf26b31c348c00ef5dfe714ee002
TechAvi-eng <n.dhulashia@gmail.com>
2023-12-06 21:15:18 +0000

    modified image_processing pipeline

--- Programming/image_processing.py
+++ Programming/image_processing.py
@@ -2,12 +2,10 @@ import os
 from PIL import Image, ImageDraw
 import pywt # PyWavelets package
 from matplotlib import pyplot as plt
-from matplotlib import colormaps
-import numpy as np
-from scipy import ndimage
+import os
+from PIL import Image
 import cv2
-import math
-
+import numpy as np
 
 def import_image(folder_path, image_name):
     # Obtaining the image path
@@ -35,8 +33,8 @@ def gray_conversion(img):
 def wavelet_coefficients(img_gray):
     
     # Wavlet decomposition (2 level)
-    n = 2 # level of decomposition
-    w = 'db1' # mother wavelet type
+    n = 1 # level of decomposition
+    w = 'db18' # mother wavelet type
 
     coeffs = pywt.wavedec2(img_gray, wavelet=w, level=n) # perform wavelet decompositionq
 
@@ -58,7 +56,7 @@ def wavelet_coefficients(img_gray):
 def wavelet_decomposition_removal(img_gray):
 
     # Wavlet decomposition (2 level)
-    n = 2 # level of decomposition
+    n = 4 # level of decomposition
     w = 'haar' # mother wavelet type 
 
     coeffs = pywt.wavedec2(img_gray, wavelet=w, level=n)
@@ -67,7 +65,7 @@ def wavelet_decomposition_removal(img_gray):
 
     Csort = np.sort(np.abs(coeff_arr.reshape(-1))) # sort the wavelet coefficients in ascending order
 
-    # for keep in (0.2, 0.1, 0.05, 0.01, 0.005): # top 10%, 5%, 1%, 0.5% of wavelet coefficients are kept
+    # for keep in (0.2, 0.1, 0.05, 0.01, 0.001): # top 10%, 5%, 1%, 0.5% of wavelet coefficients are kept
     #     thresh = Csort[int(np.floor((1-keep)*len(Csort)))] # calculate the threshold value
     #     ind = np.abs(coeff_arr) > thresh # find the indices of the wavelet coefficients that are greater than the threshold
     #     Cfilt = coeff_arr * ind # filter out the wavelet coefficients that are less than the threshold
@@ -87,17 +85,17 @@ def wavelet_decomposition_removal(img_gray):
     keep = 0.05
     thresh = Csort[int(np.floor((1-keep)*len(Csort)))]
     ind = np.abs(coeff_arr) > thresh
-    Cfilt = coeff_arr * ind
+    Cfilt = coeff_arr * ind # filter out the wavelet coefficients that are less than the threshold
 
-    coeffs_filt = pywt.array_to_coeffs(Cfilt, coeffs_slices, output_format='wavedec2')
+    coeffs_filt = pywt.array_to_coeffs(Cfilt, coeffs_slices, output_format='wavedec2') # convert the filtered wavelet coefficients back to the format used by the pywt package
 
-    Arecon = pywt.waverec2(coeffs_filt, wavelet=w)
+    Arecon = pywt.waverec2(coeffs_filt, wavelet=w) # perform inverse wavelet transform to reconstruct the image
 
     # Store the reconstructed image as img_post_wavelet
     img_post_wavelet = Image.fromarray(Arecon.astype('uint8'))
 
     # Display the reconstructed image
-    #img_post_wavelet.show()
+    # img_post_wavelet.show()
 
     return img_post_wavelet
 
@@ -113,96 +111,12 @@ def bw_conversion(img):
     #img_bw.show()
 
     return img_bw
-    
-
-def cell_boundary_reconstruction(img):
-    dilation_radius = 4
-    erosion_radius = 10
-
-    bw_img_array = np.array(img)
-
-    # Perform dilation to fill gaps and make boundaries smoother
-    dilated_img = ndimage.binary_dilation(bw_img_array, structure=np.ones((dilation_radius, dilation_radius)))
-
-    # Perform erosion to refine the boundaries and make them more circular
-    eroded_img = ndimage.binary_erosion(dilated_img, structure=np.ones((erosion_radius, erosion_radius)))
-
-    # Create a new PIL Image from the NumPy array
-    reconstructed_img = Image.fromarray(eroded_img.astype(np.uint8) * 255)
-
-    # Display the original, black and white, and reconstructed images if needed
-    #reconstructed_img.show(title='Reconstructed Image')
-
-    # Save the image in testing_images folder as 'reconstructured_image.png'
-    reconstructed_img.save('Programming/testing_images/reconstructed_image.png')
-
-    return reconstructed_img
 
 
-def cell_counting(img):
-    img_array = np.array(img)
-
-    # Find contours in the smoothed image
-    contours, _ = cv2.findContours(img_array, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Create a copy of the image for visualization purposes
-    img_with_contours = img.copy()
-
-    # Draw the contours on the image
-    cv2.drawContours(np.array(img_with_contours), contours, -1, (0, 255, 0), 2)
-
-    # Create a copy of the image for visualization purposes
-    img_with_circles = img.copy()
-
-    # Draw circles around the identified cells
-    draw = ImageDraw.Draw(img_with_circles)
-    for contour in contours:
-        # Fit a circle around each contour
-        center, radius = cv2.minEnclosingCircle(contour)
-        center = tuple(map(int, center))
-        radius = int(radius)
-        
-        # Draw the circle on the image
-        draw.ellipse([center[0] - radius, center[1] - radius, center[0] + radius, center[1] + radius])
-
-    # Display the original image and image with circles if needed
-    img_with_circles.show(title='Image with Circles')
-
-    # Count the number of detected contours (cells)
-    num_cells = len(contours)
-    print(f"Number of cells: {num_cells}")
-
-    return num_cells
-
-def cell_counting_2(img):
-
-    img_array = np.array(img)
-
-    cnts = cv2.findContours(img_array, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-    minimum_area = 1
-    average_cell_area = 650
-    connected_cell_area = 650
-    cells = 0
-    
-    image_original = cv2.imread("Programming/testing_images/3_00013.png")
-    original = image_original.copy()
-
-    for c in cnts:
-        area = cv2.contourArea(c)
-        if area > minimum_area:
-            cv2.drawContours(original, [c], -1, (36,255,12), 2)
-            if area > connected_cell_area:
-                cells += math.ceil(area / average_cell_area)
-            else:
-                cells += 1
-    print('Cells: {}'.format(cells))
-    
-    # Show the image with the identified cells
-    cv2.imshow('original', original)
-    cv2.waitKey()
-
+def cell_counting(img_bw):
+    """
+    This function counts the number of cells in the image, by adding ellipses around all circles detected in the image, even if they have gaps. This function then shows the image with the ellipses drawn around the circles.
+    """
 
 def main():
     # Set the path to the folder containing the image
@@ -224,16 +138,10 @@ def main():
     img_post_wavelet = wavelet_decomposition_removal(img_gray)
 
     # Display the reconstructed image
-    bw_converted = bw_conversion(img_post_wavelet)
-
-    # Reconstructing image boundaries
-    img_reconstructed = cell_boundary_reconstruction(bw_converted)
+    img_bw = bw_conversion(img_post_wavelet)
 
     # Counting the number of cells
-    #cell_count = cell_counting(img_reconstructed)
-
-    # Counting the number of cells
-    cell_counting_2(img_reconstructed)
+    cell_count = cell_counting(img_bw)
 
 if __name__ == "__main__":
     main()
