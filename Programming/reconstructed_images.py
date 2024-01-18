3af55e56a1b0deeadfd403d6f16dece90625c2de
TechAvi-eng <n.dhulashia@gmail.com>
2024-01-18 17:00:12 +0000

    performance graphs from approximation coefficients

--- Programming/reconstructed_images.py
+++ Programming/reconstructed_images.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 from PIL import Image
+from skimage.restoration import (denoise_wavelet, estimate_sigma)
+from skimage.metrics import (peak_signal_noise_ratio, structural_similarity)
+import skimage.io
 
 
 folder_path = 'Programming/images'
@@ -15,7 +18,6 @@ imArray = cv2.imread(image_path)
 cv2.imshow('Image', imArray)
 cv2.waitKey(0) 
 cv2.destroyAllWindows()
-
 
 imArrayG = cv2.cvtColor(imArray, cv2.COLOR_BGR2GRAY)
 
@@ -29,20 +31,31 @@ coeffs_A = list(coeffs)
 for i in range(1, n+1):
     coeffs_A[i] = tuple(np.zeros_like(element) for element in coeffs[i])
 
-reconstructed_image = pywt.waverec2(tuple(coeffs_A), 'haar')
+reconstructed_image_A = pywt.waverec2(tuple(coeffs_A), wavelet)
+reconstructed_image_A = np.uint8(reconstructed_image_A)
 
-cv2.imshow('Approx Coefficients Reconstructed Image', reconstructed_image.astype(np.uint8))
+cv2.imshow('Approx Coefficients Reconstructed Image', reconstructed_image_A)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # SETTING APPROXIMATION COEFFICIENTS TO ZERO
-
 coeffs_D = list(coeffs)
 for i in range(0, 1):
     coeffs_D[i] = tuple(np.zeros_like(element) for element in coeffs[i])
 
-reconstructed_image = pywt.waverec2(tuple(coeffs_D), 'haar')
+reconstructed_image_D = pywt.waverec2(tuple(coeffs_D), wavelet)
+reconstructed_image_D = np.uint8(reconstructed_image_D)
 
-cv2.imshow('Detail Coefficients Reconstructed Image', reconstructed_image.astype(np.uint8))
+cv2.imshow('Detail Coefficients Reconstructed Image', reconstructed_image_D.astype(np.uint8))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Use a for loop to calculate the PSNR and SSIM of the reconstrcuted_image_A for db1 through to db20, plotting the results on the same axis
+
+
+# DENOISING PERFORMANCE METRICS
+psnr_A = peak_signal_noise_ratio(imArrayG, reconstructed_image_A)
+print('PSNR of Approx Coefficients: ', psnr_A)
+
+ssim_A = structural_similarity(imArrayG, reconstructed_image_A)
+print('SSIM of Approx Coefficients: ', ssim_A)
